593d25c4103934f61d7b93e39392558ff4520303
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2019-09-20 15:13:50 -0300

    ejs receiving data in template

--- controllers/prController.js
+++ controllers/prController.js
@@ -1,7 +1,27 @@
+let data = [{
+    id: '1',
+    description: 'Ação One Page',
+    area: 'Trade',
+    date: '09/09/2019',
+    originalValue: 'R$ 19.542,84',
+    prValue: '',
+    ressarcimentValue: '',
+    ressarcimentValue: ''
+},{
+    id: '1',
+    description: 'Ação Two Page',
+    area: 'Trade',
+    date: '09/09/2019',
+    originalValue: 'R$ 19.542,84',
+    prValue: '',
+    ressarcimentValue: '',
+    ressarcimentValue: ''
+}]
+
 module.exports = (app) => {
-    
+
     app.get('/', (req, res) => {
-        res.render('./index.ejs')
+        res.render('index', { data: data })
     })
 
 }
