c532557fdcc77827aac1802e4ef5c9a952db5600
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2019-09-20 14:13:25 -0300

    Added view and controller

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const jsController = require('./controllers/prController')
+
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.render('./index.ejs')
-})
-
 app.listen(port)
-console.log('You are listening on the port ' + port);
+console.log('You are listening on the port ' + port)
+
+jsController(app)
